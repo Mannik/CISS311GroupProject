ac808e6d907e8e25d073ba5d8c443b7f3e1fda06
Mannik <zbowen@null.net>
2021-02-19 15:45:36 -0600

    Created basic main form to allow linking to sub forms. Added Icon and a couple images to spruce things up.
    Signed-off-by: Mannik <Zachary Bowen>

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace CISS311GroupProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
