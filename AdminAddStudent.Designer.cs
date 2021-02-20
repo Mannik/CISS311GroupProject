961ee68d28570056212df5bb353a6740259f7396
Brent McKinney <me@brentemckinney.com>
2021-02-20 10:29:21 -0600

    add "adminaddstudent" form
    added a form (adminaddstudent), added form elements, created event handlers, added todo comments in the code view

---
+++ AdminAddStudent.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace CISS311GroupProject
+{
+    partial class AdminAddStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSaveAddAnother = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.cbIncomingCredits = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbCreditHours = new System.Windows.Forms.Label();
+            this.lblAddNewStudent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(36, 323);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(115, 35);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "&Save && Close";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSaveAddAnother
+            // 
+            this.btnSaveAddAnother.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveAddAnother.Location = new System.Drawing.Point(174, 323);
+            this.btnSaveAddAnother.Name = "btnSaveAddAnother";
+            this.btnSaveAddAnother.Size = new System.Drawing.Size(115, 35);
+            this.btnSaveAddAnother.TabIndex = 1;
+            this.btnSaveAddAnother.Text = "Save && &New";
+            this.btnSaveAddAnother.UseVisualStyleBackColor = true;
+            this.btnSaveAddAnother.Click += new System.EventHandler(this.btnSaveAddAnother_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(312, 323);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(115, 35);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFirstName.Location = new System.Drawing.Point(146, 73);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(226, 26);
+            this.tbFirstName.TabIndex = 3;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.Location = new System.Drawing.Point(48, 76);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(77, 19);
+            this.lblFirstName.TabIndex = 4;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(47, 117);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(76, 19);
+            this.lblLastName.TabIndex = 5;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLastName.Location = new System.Drawing.Point(146, 114);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(226, 26);
+            this.tbLastName.TabIndex = 6;
+            // 
+            // cbIncomingCredits
+            // 
+            this.cbIncomingCredits.AutoSize = true;
+            this.cbIncomingCredits.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbIncomingCredits.Location = new System.Drawing.Point(52, 197);
+            this.cbIncomingCredits.Name = "cbIncomingCredits";
+            this.cbIncomingCredits.Size = new System.Drawing.Size(203, 23);
+            this.cbIncomingCredits.TabIndex = 7;
+            this.cbIncomingCredits.Text = "Student has incoming credits.";
+            this.cbIncomingCredits.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(146, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // lbCreditHours
+            // 
+            this.lbCreditHours.AutoSize = true;
+            this.lbCreditHours.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCreditHours.Location = new System.Drawing.Point(47, 158);
+            this.lbCreditHours.Name = "lbCreditHours";
+            this.lbCreditHours.Size = new System.Drawing.Size(88, 19);
+            this.lbCreditHours.TabIndex = 9;
+            this.lbCreditHours.Text = "Credit Hours";
+            // 
+            // lblAddNewStudent
+            // 
+            this.lblAddNewStudent.AutoSize = true;
+            this.lblAddNewStudent.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddNewStudent.Location = new System.Drawing.Point(36, 28);
+            this.lblAddNewStudent.Name = "lblAddNewStudent";
+            this.lblAddNewStudent.Size = new System.Drawing.Size(210, 31);
+            this.lblAddNewStudent.TabIndex = 10;
+            this.lblAddNewStudent.Text = "Add New Student";
+            // 
+            // AdminAddStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 403);
+            this.Controls.Add(this.lblAddNewStudent);
+            this.Controls.Add(this.lbCreditHours);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cbIncomingCredits);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSaveAddAnother);
+            this.Controls.Add(this.btnSave);
+            this.Name = "AdminAddStudent";
+            this.Text = "Administrator: Add Student";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSaveAddAnother;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.CheckBox cbIncomingCredits;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbCreditHours;
+        private System.Windows.Forms.Label lblAddNewStudent;
+    }
+}
---
+++ AdminAddStudent.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CISS311GroupProject
+{
+    public partial class AdminAddStudent : Form
+    {
+        public AdminAddStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //todo 
+            //call save method then close or cancel back to administrator form
+            SaveFormData();
+            ExitForm();
+        }
+
+        private void btnSaveAddAnother_Click(object sender, EventArgs e)
+        {
+            //todo 
+            //call save method then clear form
+            SaveFormData();
+            ClearForm();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            //todo 
+            //cancel back to the administrator form
+            ExitForm();
+        }
+
+        private void SaveFormData()
+        {
+            //todo 
+            //collect form data and input it to the database
+        }
+
+        private void ClearForm()
+        {
+            //todo 
+            //clear data from the form
+        }
+
+        private void ExitForm()
+        {
+            //todo 
+            //cancel or unload form and return to the administrator form
+        }
+    }
+}
