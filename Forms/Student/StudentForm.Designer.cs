79846ae4339e0b43d1b2781e0141e7feb98c7864
Brent McKinney <me@brentemckinney.com>
2021-03-03 23:51:58 -0600

    check if student can graduate
    Added a student find field to the student section of the program. Right now when a student types their student ID into the textbox and clicks find it checks to see if they have enough credits to graduate. This will eventually (or that's the plan) load in the student information for use on each of the student sections.

--- Forms/Student/StudentForm.Designer.cs
+++ Forms/Student/StudentForm.Designer.cs
@@ -29,32 +29,68 @@
         private void InitializeComponent()
         {
             this.enrollButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.studentIdTextBox = new System.Windows.Forms.TextBox();
+            this.findButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // enrollButton
             // 
-            this.enrollButton.Location = new System.Drawing.Point(537, 341);
+            this.enrollButton.Location = new System.Drawing.Point(43, 367);
             this.enrollButton.Name = "enrollButton";
-            this.enrollButton.Size = new System.Drawing.Size(75, 23);
+            this.enrollButton.Size = new System.Drawing.Size(248, 53);
             this.enrollButton.TabIndex = 0;
             this.enrollButton.Text = "Enroll";
             this.enrollButton.UseVisualStyleBackColor = true;
             this.enrollButton.Click += new System.EventHandler(this.enrollButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Student ID:";
+            // 
+            // studentIdTextBox
+            // 
+            this.studentIdTextBox.Location = new System.Drawing.Point(96, 20);
+            this.studentIdTextBox.Name = "studentIdTextBox";
+            this.studentIdTextBox.Size = new System.Drawing.Size(100, 20);
+            this.studentIdTextBox.TabIndex = 2;
+            // 
+            // findButton
+            // 
+            this.findButton.Location = new System.Drawing.Point(234, 18);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(75, 23);
+            this.findButton.TabIndex = 3;
+            this.findButton.Text = "&Find";
+            this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
             // 
             // StudentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.findButton);
+            this.Controls.Add(this.studentIdTextBox);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.enrollButton);
             this.Name = "StudentForm";
             this.Text = "StudentForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button enrollButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox studentIdTextBox;
+        private System.Windows.Forms.Button findButton;
     }
 }
